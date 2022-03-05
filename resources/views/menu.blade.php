c3b63e387e39ecded428d557bac11749bbb96694
zeyniep <75210764+zeyniep@users.noreply.github.com>
2022-03-05 17:01:48 +0300

    first

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <link rel="stylesheet" href="{{asset('css/tasarim.css')}}" type="text/css">
+        <div class= "row">
+            <div class ="row"></div>
+            <div class="col-1">
+            <div >
+    <ul >
+      <li><a href="/ogrencianasayfa">ANA SAYFA</a></li>
+      <li><a href="/basvurularim">FİLMLER</a></li>
+      <li><a href="/basvuruyap">SAATLER</a></li>
+      <li><a href="/kisiselbilgiler">BİLDİRİMLER</a></li>
+      <li><a href="/">ÇIKIŞ</a></li>
+    </ul>
+  </div>
+            </div>
+        </div>
+</body>
+</html>
