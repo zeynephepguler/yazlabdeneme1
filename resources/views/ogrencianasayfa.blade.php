94fbf5d9dc5675157bc5bb6f329a0be19f731210
zeyniep <75210764+zeyniep@users.noreply.github.com>
2022-03-06 14:59:35 +0300

    ileti

--- resources/views/ogrencianasayfa.blade.php
+++ resources/views/ogrencianasayfa.blade.php
@@ -4,15 +4,52 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Proje Takip Sistemi</title>
 </head>
+
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="col-align-self-center">
-                asdsdad
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+   
+    <div class="container"><br><br><br><br><br><br><br><br>
+        
+        <div class="row ">
+            <div class="col-2"></div>
+            <div class="col-3">
+                    <button type="submit" class="button" style="background-color: rgb(221, 208, 23);">PROJE <br> BASLIGI <input type="text" placeholder="baslık giriniz"></button>
             </div>
-        </div>
+            <div class="col-3">
+                
+                    <button type="submit" class="button" style="background-color: rgb(253, 160, 53);">AMAC <br>ONEM <br>KAPSAM</button>
+            </div>
+            <div class="col-2">
+                    <button type="submit" class="button" style="background-color: rgb(255, 41, 13);">METARYEL <br>YONTEM <br>ARASTIRMA OLANAKLARI</button>
+           
+        </div><br>
+    </div><br>
+    <div class="row">
+        <div class="col-4"><br></br></div>
+        
+        <div class="col"><h3>Projenizle İlişkili 5 kelime giriniz</h3></div>
+    
     </div>
+        <div class="row">
+            <div class="col-4">
+                
+            </div>
+            <div class="col-2"><input type="text" placeholder="kelime giriniz">
+            <div class="col"><input type="text" placeholder="kelime giriniz"></div>
+            <div class="col"><input type="text" placeholder="kelime giriniz"></div>
+            <div class="col"><input type="text" placeholder="kelime giriniz"></div>
+            <div class="col"><input type="text" placeholder="kelime giriniz"></div>
+        </div>
+            
+            <div class="col-4" ><br><button class="ibuton">Kontrol et</button>
+            <div class="col"><button class="ibuton" style="background-color:red">Benzerlerini Goster</button></div>
+            
+         </div>
+            <div class="col"></div>
+        </div>
+    
 </body>
+
 </html>
